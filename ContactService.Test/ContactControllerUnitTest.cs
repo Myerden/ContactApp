ebbfa97ee4faa217e700577d3d7a216e2628d7f7
M. Yusuf ERDEN <muhammedyusuferden@gmail.com>
2021-11-13 16:11:30 +0300

    Add contact unit test methods

--- ContactService.Test/ContactControllerUnitTest.cs
+++ ContactService.Test/ContactControllerUnitTest.cs
@@ -2,7 +2,9 @@
 using ContactService.Api.Configuration;
 using ContactService.Api.Controllers;
 using ContactService.Api.Data;
+using ContactService.Api.Model;
 using ContactService.Api.Repository;
+using ContactService.Application.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -32,6 +34,8 @@ namespace ContactService.Test
         {
             var context = new ContactContext(dbContextOptions);
 
+            context.Database.Migrate();
+
             repository = new ContactRepository(context);
 
             var mapperConfig = new MapperConfiguration(mc =>
@@ -40,6 +44,14 @@ namespace ContactService.Test
             });
 
             mapper = mapperConfig.CreateMapper();
+
+            //insert demo data
+            ContactData.DEMO.ForEach(c =>
+            {
+                var contact = mapper.Map<Contact>(c);
+                var insertedId = repository.Create(contact).ConfigureAwait(false).GetAwaiter().GetResult();
+            });
+
         }
 
         #region Insert 
@@ -47,27 +59,124 @@ namespace ContactService.Test
         [Fact]
         public async void InsertContact_ShouldReturnOkResult()
         {
-            //Arrange  
             var controller = new ContactController(repository, mapper);
 
-            //Act  
-            var data = await controller.Post(ContactData.VALID);
+            var data = await controller.Post(ContactData.VALID) as OkObjectResult;
 
-            //Assert  
             Assert.IsType<OkObjectResult>(data);
+
+            var obj = data.Value as ContactDto;
+
+            Assert.NotNull(obj);
+
+            Assert.Equal(ContactData.VALID.FirstName, obj.FirstName);
+            Assert.Equal(ContactData.VALID.LastName, obj.LastName);
         }
 
         [Fact]
         public async void InsertContact_ShouldReturnBadRequest()
         {
-            //Arrange  
             var controller = new ContactController(repository, mapper);
-
-            //Act              
+        
             var data = await controller.Post(ContactData.UNVALID);
 
-            //Assert  
             Assert.IsType<BadRequestResult>(data);
+        }
+
+        #endregion
+
+        #region Get
+
+        [Fact]
+        public async void GetContact_ShouldReturnOkResult()
+        {
+            var controller = new ContactController(repository, mapper);
+
+            var id = ContactData.DEMO[0].Id;
+
+            var data = await controller.Get(id);
+
+            Assert.IsType<OkObjectResult>(data);
+        }
+
+        [Fact]
+        public async void GetContact_ShouldReturnNotFoundResult()
+        {
+            var controller = new ContactController(repository, mapper);
+
+            var id = Guid.NewGuid();
+
+            var data = await controller.Get(id);
+
+            Assert.IsType<NotFoundResult>(data);
+        }
+
+        [Fact]
+        public async void GetContact_ShouldMatchResult()
+        {
+            var controller = new ContactController(repository, mapper);
+
+            var demo = ContactData.DEMO[0];
+
+            var data = await controller.Get(demo.Id) as OkObjectResult;
+
+            Assert.IsType<OkObjectResult>(data);
+
+            var obj = data.Value as ContactDto;
+
+            Assert.NotNull(obj);
+
+            Assert.Equal(demo.FirstName, obj.FirstName);
+            Assert.Equal(demo.LastName, obj.LastName);
+            Assert.Equal(demo.Company, obj.Company);
+
+            demo.ContactDetails.ForEach(demoContactDetail =>
+            {
+                var contactDetail = obj.ContactDetails.Find(cd => cd.Id == demoContactDetail.Id);
+
+                Assert.Equal(demoContactDetail.ContactDetailType, contactDetail.ContactDetailType);
+                Assert.Equal(demoContactDetail.ContactDetailContent, contactDetail.ContactDetailContent);
+            });
+        }
+
+        #endregion
+
+        #region Get All  
+
+        [Fact]
+        public async void GetAllContact_ShouldReturnOkResult()
+        {
+            var controller = new ContactController(repository, mapper);
+
+            var data = await controller.Get();
+
+            Assert.IsType<OkObjectResult>(data);
+        }
+
+        [Fact]
+        public async void GetAllContact_ShouldMatchResult()
+        {
+            var controller = new ContactController(repository, mapper);
+
+            var data = await controller.Get() as OkObjectResult;
+
+            Assert.IsType<OkObjectResult>(data);
+
+            var obj = data.Value as List<ContactDto>;
+
+            Assert.NotNull(obj);
+
+            ContactData.DEMO.ForEach(demoContact =>
+            {
+                var contact = obj.Find(c => c.Id == demoContact.Id);
+
+                Assert.NotNull(contact);
+
+                Assert.Equal(demoContact.FirstName, contact.FirstName);
+                Assert.Equal(demoContact.LastName, contact.LastName);
+                Assert.Equal(demoContact.Company, contact.Company);
+            });
+
         }
 
         #endregion
--- ContactService.Test/ContactData.cs
+++ ContactService.Test/ContactData.cs
@@ -17,20 +17,49 @@ namespace ContactService.Test
             ContactDetails = new List<ContactDetailDto>()
             {
                 new ContactDetailDto(){ ContactDetailType = ContactDetailType.PHONE, ContactDetailContent = "0555 555 5555" },
-                new ContactDetailDto(){ ContactDetailType = ContactDetailType.EMAIL, ContactDetailContent = "example@example.org" },
-                new ContactDetailDto(){ ContactDetailType = ContactDetailType.LOCATION, ContactDetailContent = "Istanbul" },
+                new ContactDetailDto(){ ContactDetailType = ContactDetailType.EMAIL, ContactDetailContent = "example1@example.org" },
+                new ContactDetailDto(){ ContactDetailType = ContactDetailType.LOCATION, ContactDetailContent = "İstanbul" },
             }
         };
 
         public static ContactDto UNVALID = new ContactDto()
         {
             FirstName = "Yusuf",
-            Company = "ABC Ltd. Şti.",
+            Company = "DEF Ltd. Şti.",
             ContactDetails = new List<ContactDetailDto>()
             {
-                new ContactDetailDto(){ ContactDetailType = ContactDetailType.PHONE, ContactDetailContent = "0555 555 5555" },
-                new ContactDetailDto(){ ContactDetailType = ContactDetailType.EMAIL, ContactDetailContent = "example@example.org" },
-                new ContactDetailDto(){ ContactDetailType = ContactDetailType.LOCATION, ContactDetailContent = "Istanbul" },
+                new ContactDetailDto(){ ContactDetailType = ContactDetailType.PHONE, ContactDetailContent = "0850 124 3322" },
+                new ContactDetailDto(){ ContactDetailType = ContactDetailType.EMAIL, ContactDetailContent = "example2@example.org" },
+                new ContactDetailDto(){ ContactDetailType = ContactDetailType.LOCATION, ContactDetailContent = "Ankara" },
+            }
+        };
+
+        public static List<ContactDto> DEMO = new List<ContactDto>() {
+            new ContactDto()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Muhammed",
+                LastName = "Erden",
+                Company = "XYZ Ltd. Şti.",
+                ContactDetails = new List<ContactDetailDto>()
+                {
+                    new ContactDetailDto(){ Id = Guid.NewGuid(), ContactDetailType = ContactDetailType.PHONE, ContactDetailContent = "0212 333 4455" },
+                    new ContactDetailDto(){ Id = Guid.NewGuid(), ContactDetailType = ContactDetailType.EMAIL, ContactDetailContent = "example3@example.org" },
+                    new ContactDetailDto(){ Id = Guid.NewGuid(), ContactDetailType = ContactDetailType.LOCATION, ContactDetailContent = "İzmir" },
+                }
+            },
+            new ContactDto()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Mike",
+                LastName = "Tyson",
+                Company = "Tyson Lojistik ve Taşımacılık",
+                ContactDetails = new List<ContactDetailDto>()
+                {
+                    new ContactDetailDto(){ Id = Guid.NewGuid(), ContactDetailType = ContactDetailType.PHONE, ContactDetailContent = "0545 123 4545" },
+                    new ContactDetailDto(){ Id = Guid.NewGuid(), ContactDetailType = ContactDetailType.EMAIL, ContactDetailContent = "tyson@example.org" },
+                    new ContactDetailDto(){ Id = Guid.NewGuid(), ContactDetailType = ContactDetailType.LOCATION, ContactDetailContent = "New York" },
+                }
             }
         };
     }
